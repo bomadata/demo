3608bc9e5780f8c97c2aca67cacc660669781fa9
Boma <bomadata2@gmail.com>
2024-04-24 11:28:19 -0500

    adding Untitled 8.py

---
+++ Untitled-8.py
@@ -0,0 +1,29 @@
+print('Welcome to my Computer Quiz')
+
+playing = input('Do you want to play? ')
+
+if playing != 'yes':
+    quit()
+    
+print("Ok! Let's play :)")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphic processing unit":
+    print("correct!")
+
+else:
+    print("incorrect!")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("correct!")
+
+else:
+    print("incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphic processing unit":
+    print("correct!")
+
+else:
+    print("incorrect!")
